c813d08ee8cf510d25e1df2f05ba505f5bd5db7a
dave-s <dave.s@kakaocorp.com>
2020-06-08 16:23:23 +0900

    Updates

---
+++ precache-manifest.afdc265c8e33c10052919f7134af5361.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa55ec2cd54f659052d83f3ba4906c3",
+    "url": "/pages.git/index.html"
+  },
+  {
+    "revision": "cd4b7a392920d26cd3e7",
+    "url": "/pages.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f10a602a14f46f65bed2",
+    "url": "/pages.git/static/js/2.41509f0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pages.git/static/js/2.41509f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4b7a392920d26cd3e7",
+    "url": "/pages.git/static/js/main.fe91a090.chunk.js"
+  },
+  {
+    "revision": "0c2d51398ddf7bfff147",
+    "url": "/pages.git/static/js/runtime-main.38843792.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pages.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
